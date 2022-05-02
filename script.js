9355624c8e49182069bd84e605be01d48d12f724
Dana <dana.hong@hotmail.com>
2022-05-02 10:36:10 -0400

    Change UI proportions and add turn indicator

--- script.js
+++ script.js
@@ -14,27 +14,28 @@ let Player = function(name, marker, turn) {
     return { name, marker, turn, placeMarker, startTurn, endTurn };
 }
 
-let playerOne = Player('Player1', 'X', true);
-let playerTwo = Player('Player2', 'O', false);
+let playerOne = Player('Player One', 'X', true);
+let playerTwo = Player('Player Two', 'O', false);
+document.querySelector('.current-player-turn').textContent = `${playerOne.name}'s turn!`;
 
 let Square = function(DOMObject) {
     DOMObject.status = false;
     let handleEvent = () => {
         if (DOMObject.status) {
-            return console.log('square filled already', playerOne.turn, `testing:`, DOMObject.status);
+            return;
         } else if (!DOMObject.status) {
             if (playerOne.turn) {
+                document.querySelector('.current-player-turn').textContent = `${playerTwo.name}'s turn!`;
                 playerOne.placeMarker(DOMObject);
                 playerOne.turn = playerOne.endTurn();
                 playerTwo.turn = playerTwo.startTurn();
                 DOMObject.status = true;
-                console.log('hey1');
             } else if (playerTwo.turn) {
+                document.querySelector('.current-player-turn').textContent = `${playerOne.name}'s turn!`;
                 playerTwo.placeMarker(DOMObject);
                 playerTwo.turn = playerTwo.endTurn();
                 playerOne.turn = playerOne.startTurn();
                 DOMObject.status = true;
-                console.log('hey2');
             }
         }
     }
