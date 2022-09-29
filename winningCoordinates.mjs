dd913dd136d1f378b884d2e1eff41ab791aeb62c
Dana <dana.hong@hotmail.com>
2022-09-28 21:05:12 -0400

    Create data representing winning positions

---
+++ winningCoordinates.mjs
@@ -0,0 +1,12 @@
+const winningCoordinates = {
+    topRow: ['0', '1', '2'],
+    middleRow: ['3', '4', '5'],
+    bottomRow: ['6', '7', '8'],
+    leftCol: ['0', '3', '6'],
+    middleCol: ['1', '4', '7'],
+    rightCol: ['2', '5', '8'],
+    topLeftDiag: ['0', '4', '8'],
+    topRightDiag: ['2', '4', '6']
+}
+
+export default winningCoordinates;
