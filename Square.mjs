4df17aecc676c0ea6d801034fc4ae115952112ba
Dana <dana.hong@hotmail.com>
2022-09-28 22:06:43 -0400

    Add id property

--- Square.mjs
+++ Square.mjs
@@ -2,6 +2,5 @@ export default function Square(id) {
     const square = document.createElement('button');
     square.classList.add('square');
     square.id = id;
-    document.body.append(square);
     return square;
 }
