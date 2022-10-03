d5c02ac63bb327bc8311dac0f2e57264987f1c81
Dana <dana.hong@hotmail.com>
2022-10-03 15:05:37 -0400

    Add container elements to object

--- Gameboard.mjs
+++ Gameboard.mjs
@@ -2,11 +2,16 @@ import Square from "./Square.mjs";
 
 export default function Gameboard() {
     const gameBoard = {
-        gameStarted: false,
+        player1: null,
+        player2: null,
+        currentPlayer: null,
         gameOver: false,
         winner: null,
+        gameBoardContainerElement: document.createElement('div'),
         gameBoardElement: document.createElement('div'),
-        resetBtn: document.createElement('button')
+        gameBoardMessageElement: document.createElement('p'),
+        resetBtn: document.createElement('button'),
+        backToMainBtn: document.createElement('button')
     }
 
     let squares = new Array(9).fill("");
@@ -15,10 +20,23 @@ export default function Gameboard() {
         gameBoard.gameBoardElement.append(square);
     });
 
+    const gameTitle = document.createElement('h1');
+    gameTitle.classList.add('game-title');
+    gameBoard.gameBoardMessageElement.classList.add('game-message');
+    gameBoard.gameBoardContainerElement.classList.add('container');
 
-    document.querySelector('.container').append(gameBoard.gameBoardElement, gameBoard.resetBtn);
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('btn-container');
+    buttonContainer.append(gameBoard.resetBtn, gameBoard.backToMainBtn);
+    gameBoard.gameBoardContainerElement.append(gameTitle, gameBoard.gameBoardMessageElement, gameBoard.gameBoardElement, buttonContainer);
+    document.body.append(gameBoard.gameBoardContainerElement);
+
     gameBoard.resetBtn.textContent = 'Reset';
     gameBoard.resetBtn.disabled = true;
+    gameBoard.resetBtn.classList.add('btn', 'invisible');
+    gameBoard.backToMainBtn.textContent = 'Back to Main Menu';
+    gameBoard.backToMainBtn.disabled = true;
+    gameBoard.backToMainBtn.classList.add('btn', 'invisible');
     gameBoard.gameBoardElement.classList.add('gameboard');
 
     return gameBoard;
